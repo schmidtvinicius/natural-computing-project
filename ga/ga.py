99cd300bcb57b929c9e810493f83321514f8a6d9
Ricardo Furbino <ricardofurbino@gmail.com>
2024-05-12 13:57:22 +0200

    feat: implements plot of viz evolution

--- ga/ga.py
+++ ga/ga.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import tqdm
 
@@ -38,8 +39,16 @@ class GeneticAlgorithm(ABC):
     def calculate_fitness(self, individual: list[tuple[str, int]]) -> float:
         pass
 
-    def evolve(self, verbose: bool = False) -> list[tuple[str, int]]:
+    def evolve(
+        self,
+        verbose: bool = False,
+        plot_generations: bool = False,
+        return_generations_scores: bool = False
+    ) -> list[tuple[str, int]] | tuple[list[tuple[str, int]], list[float]]:
         population = self.initialize_population()
+
+        if plot_generations or return_generations_scores:
+            best_scores = []
 
         for generation in range(self.num_generations) if not verbose else tqdm.tqdm(range(self.num_generations)):
             next_generation = []
@@ -60,9 +69,24 @@ class GeneticAlgorithm(ABC):
             if verbose:
                 print(f'Generation {generation + 1} best fitness score: {self.calculate_fitness(min(population, key=self.calculate_fitness))}')
 
-        # Return the best individual from the final population
+            if plot_generations or return_generations_scores:
+                best_scores.append(self.calculate_fitness(min(population, key=self.calculate_fitness)))
+
+        if plot_generations:
+            self.plot_evolution(best_scores)
+
+        if return_generations_scores:
+            return min(population, key=self.calculate_fitness), best_scores
         return min(population, key=self.calculate_fitness)
 
     def select_parent(self, population: list[list[tuple[str, int]]], first: bool = True) -> list[tuple[str, int]]:
         # get best parent if first is True, else get second best parent
         return min(population, key=self.calculate_fitness) if first else sorted(population, key=self.calculate_fitness)[1]
+    
+    def plot_evolution(self, best_scores: list[float]) -> None:
+
+        plt.plot(best_scores)
+        plt.xlabel('Generation')
+        plt.ylabel('Fitness Score')
+        plt.title('Fitness Score over Generations')
+        plt.savefig('fitness_score.png')
